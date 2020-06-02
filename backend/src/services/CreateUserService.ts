e7819de9b7e4c2767f21c62e47430e48f6148084
jhordanjes <jhordanjes@gmail.com>
2020-06-02 14:59:35 -0300

    class of error

--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -1,6 +1,9 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
+
 import User from '../models/User';
+
+import AppError from '../errors/AppError';
 
 interface Request {
   name: string;
@@ -17,7 +20,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('Email address already exists');
+      throw new AppError('Email address already exists');
     }
 
     const hashedPassword = await hash(password, 8);
