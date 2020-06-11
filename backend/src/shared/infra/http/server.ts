8cf7d81d4dcbae8ff0cf9365fc7f4272d2280d5a
jhordanjes <jhordanjes@gmail.com>
2020-06-11 10:39:30 -0300

    add arquitetura ddd

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,15 +1,20 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
-
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
